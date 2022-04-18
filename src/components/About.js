4c9acb1f1c569df8546603377f34a4a1083a65de
neilabbiebebe <neilabbie123@gmail.com>
2022-04-18 17:22:29 +0800

    anotherone

--- src/components/About.js
+++ src/components/About.js
@@ -9,12 +9,7 @@ function About() {
         Holla👋
       </p>
       <h2 className="text-base sm:text-xl text-center text-gray-600 leading-relaxed mt-4">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed commodi
-        dolorum ipsa natus vero modi doloremque quia veritatis excepturi
-        accusamus in a recusandae incidunt maxime magnam, placeat saepe eveniet
-        quibusdam voluptates atque neque perferendis dignissimos ullam!
-        Doloremque vel sequi fugit animi odit eaque, molestiae nesciunt, itaque
-        magni laboriosam esse est.
+        I'm Chrisela Jel Gaudicos Godarido. I am very good at adapting to different people. Sometimes, I take on a confident-boosting, friendly, and humorous attitude. I find this to be very helpful with friends who’ve just had a bad day. Other times I’m quiet and welcoming.I am a very kind, compassionate, sensitive. I have a tough shell but on the inside I am soft. I am also a very principled person and stand strong on what is right and wrong. I am also a very straight and honest person. I understand and know that we all put on a show sometimes and do not show who we really are or what we feel but some people do this constantly and those kinds of people I cannot stand.I Believe that everything happens in a particular reason so every steping stones I've  step I should  let my feet define my failures . I can let my failures teach me as who I am as a person.
       </h2>
       <Scroll selector="#hobby">
         <div className="mx-auto p-20">
